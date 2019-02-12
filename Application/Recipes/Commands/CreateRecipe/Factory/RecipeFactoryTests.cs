572c05abd7efbb928ee49a5627e9fd38d31ac65b
: <lukasz.kowalczyk.dev@gmail.com>
2019-02-12 23:16:39 +0100

    added nUnit to Application project and test testing RecipeFactory

---
+++ Application/Recipes/Commands/CreateRecipe/Factory/RecipeFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Recipes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Recipes.Commands.CreateRecipe.Factory
+{
+    [TestFixture]
+    public class RecipeFactoryTests
+    {
+        private RecipeFactory recipeFactory;
+
+        private const string name = "Zapiekanka";
+
+        [SetUp]
+        public void SetUp()
+        {
+            recipeFactory = new RecipeFactory();
+        }
+
+        [Test]
+        public void TestCreateShouldCreateRecipe()
+        {
+            Recipe result = recipeFactory.Create(name);
+
+            Assert.That(result.Name, Is.EqualTo(name));
+        }
+    }
+}
