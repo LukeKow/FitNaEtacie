1390cdb8981daf06cee886b13158894c833a3024
: <lukasz.kowalczyk.dev@gmail.com>
2019-02-12 12:22:19 +0100

    Added Persistance project and RecipeConfiguration class

---
+++ Persistance/Recipes/RecipeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Recipes;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Persistance.Recipes
+{
+    public class RecipeConfiguration : EntityTypeConfiguration<Recipe>
+    {
+        public RecipeConfiguration()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Name)
+                .IsRequired();
+        }
+    }
+}
