2e06849afbc12f8c444a8cefc73cd2d20301eea2
: <lukasz.kowalczyk.dev@gmail.com>
2019-02-13 22:01:36 +0100

    custom folder structure configuration

--- Presentation/App_Start/RouteConfig.cs
+++ Presentation/App_Start/RouteConfig.cs
@@ -11,6 +11,8 @@ namespace Presentation
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.RouteExistingFiles = true;
+            
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
